3ff4b6e6bd9cde0e681bfd257ab9818b2f259fa2
bharath <bharath4657@gmail.com>
2024-01-02 19:13:30 +0530

    asdkjlkasj

--- catalogue.sh
+++ catalogue.sh
@@ -49,15 +49,10 @@ VALIDATE $? "unzip is"
 cd /app &>>$LOG_FILE
 VALIDATE $? "in /app directory" 
 
-npm init
 npm install  &>>$LOG_FILE
 VALIDATE $? "dependencies installation"
-npm config list
-npm cache clean --force
 
-
-
-cp -pr /tmp/roboshop_scripting/catalogue_service /etc/systemd/system/catalogue.service &>>$LOG_FILE
+cp -pr catalogue_service /etc/systemd/system/catalogue.service &>>$LOG_FILE
 VALIDATE $? "copied successful"
 
 systemctl daemon-reload
