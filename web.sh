666cbba6224554247acfa511634017f9ed634777
bharath <bharath4657@gmail.com>
2024-01-02 20:27:28 +0530

    asdsadsa343asasdasdasdasdsa

--- web.sh
+++ web.sh
@@ -42,7 +42,7 @@ VALIDATE $? "downloading webzip"
 cd /usr/share/nginx/html
 unzip /tmp/web.zip
 VALIDATE $? "unzip is"
-cp roboshop.conf /etc/nginx/default.d/roboshop.conf  &>> $LOG_FILE
+cp -pr roboshop.conf /etc/nginx/default.d/roboshop.conf
 VALIDATE $? "copying is"
 systemctl restart nginx &>> $LOG_FILE
 VALIDATE $? "restarting ngnix is"
