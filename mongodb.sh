fcb5eb2043d95bbf1f0d6a04dc84df166fc65c68
bharath <bharath4657@gmail.com>
2024-01-02 13:16:31 +0530

    hgjjhk

---
+++ mongodb.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cp -p mangodb_service /etc/yum.repos.d/mongo.repo 
+
+ID=$(id -u)
+
+VALIDATE ()
+{
+    if [ $1 -ne 0 ]
+    then
+    echo "$2 FAILED"
+    else
+    echo "$2 SUCCESSFUL"
+}
+
+if [ $ID -ne 0 ]
+then 
+echo "please run the script with root previlages"
+exit 1
+else 
+echo "you are root user"
+fi
+
+dnf install mongodb-org -y
+
+VALIDATE $? "installation is"
+
